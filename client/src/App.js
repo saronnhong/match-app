33065baf2edbfb36a21b85c4648edf06d364d898
Saron <saronnhong@gmail.com>
2019-11-05 18:07:05 -0800

    card flip working

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,24 @@ import characters from "./mahmud.json";
 import Card from "./components/Card";
 
 class App extends Component {
+  state = {
+    open: false,
+    id: ""
+  }
+
+  cardFlip = (charId) => {
+    this.setState(state => ({ open: !state.open, id: charId }))
+  }
+  
+  
   render() {
     return (
-      <div className="container">
+      <div className="container deck">
         <div className="row">
           {characters.map(character => (
-            <Card id={character.id} url={process.env.PUBLIC_URL + '/img/' + character.imageFile} />
+            <div id={character.id} onClick={() => { this.cardFlip(character.id) }}>
+              <Card id={character.id} data-imageUrl={character.imageFile} url={(this.state.open && this.state.id === character.id) ? process.env.PUBLIC_URL + '/img/' + character.imageFile  : process.env.PUBLIC_URL + '/img/wolverine.jpg'} />
+            </div>
           ))}
         </div>
       </div>
